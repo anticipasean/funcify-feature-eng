237c50c663ea0cd63ce6e3772238bbcbbc91a023
anticipasean <spmccarron@gmail.com>
2022-03-13 21:57:06 -0700

    0.0.x: Create basic char array source sequence spilterator implementation

---
+++ funcify-feature-eng-naming/src/main/kotlin/funcify/naming/charseq/CharArraySourceSequence.kt
@@ -0,0 +1,98 @@
+package funcify.naming.charseq
+
+import java.util.Spliterator
+import java.util.Spliterator.IMMUTABLE
+import java.util.Spliterator.NONNULL
+import java.util.Spliterator.ORDERED
+import java.util.Spliterator.SIZED
+import java.util.function.Consumer
+
+
+/**
+ *
+ * @author smccarron
+ * @created 3/12/22
+ */
+internal class CharArraySourceSequence(private val charArray: CharArray,
+                                       private var index: Int = 0,
+                                       private val exclusiveLimit: Int = charArray.size,
+                                       private val characteristicsBitSet: Int = DEFAULT_CHARACTERISTICS_BITSET) : ContextualCharSequence {
+    companion object {
+        internal const val DEFAULT_CHARACTERISTICS_BITSET: Int = SIZED and NONNULL and IMMUTABLE and ORDERED
+        internal val DEFAULT_COMPARATOR: Comparator<in CharContext> by lazy { Comparator.comparing(CharContext::index) }
+        private fun charContextOfIndex(chars: CharArray,
+                                       idx: Int) =
+                when (idx) {
+                    0 -> {
+                        CharContext(chars[idx],
+                                    idx,
+                                    RelativeCharSequencePosition.FIRST_CHARACTER)
+                    }
+                    (chars.size - 1) -> {
+                        CharContext(chars[idx],
+                                    idx,
+                                    RelativeCharSequencePosition.LAST_CHARACTER)
+                    }
+                    else -> {
+                        CharContext(chars[idx],
+                                    idx,
+                                    RelativeCharSequencePosition.MIDDLE_CHARACTER)
+                    }
+                }
+    }
+
+    private fun cannotAccessArrayWithGivenParameters(): Boolean {
+        return charArray.size == 0 || exclusiveLimit <= 0 || exclusiveLimit > charArray.size || index < 0 || index >= exclusiveLimit
+    }
+
+    override fun tryAdvance(action: Consumer<in CharContext>?): Boolean {
+        if (action == null || cannotAccessArrayWithGivenParameters()) {
+            return false
+        }
+        action.accept(Companion.charContextOfIndex(charArray,
+                                                   index++))
+        return true
+    }
+
+    override fun trySplit(): Spliterator<CharContext>? {
+        if (cannotAccessArrayWithGivenParameters()) {
+            return null
+        }
+        val lo = index
+        val mid = lo + exclusiveLimit ushr 1
+        return if (lo >= mid) {
+            null
+        } else {
+            CharArraySourceSequence(charArray,
+                                    lo,
+                                    mid.also { index = it },
+                                    characteristicsBitSet)
+        }
+    }
+
+    override fun forEachRemaining(action: Consumer<in CharContext>?) {
+        if (action == null || cannotAccessArrayWithGivenParameters()) {
+            return
+        }
+        var idx: Int = index
+        do {
+            action.accept(charContextOfIndex(charArray,
+                                             idx))
+        } while (++idx < exclusiveLimit)
+    }
+
+    override fun estimateSize(): Long {
+        if (cannotAccessArrayWithGivenParameters()) {
+            return 0L
+        }
+        return (exclusiveLimit - index).toLong()
+    }
+
+    override fun characteristics(): Int {
+        return characteristicsBitSet
+    }
+
+    override fun getComparator(): Comparator<in CharContext> {
+        return DEFAULT_COMPARATOR
+    }
+}
