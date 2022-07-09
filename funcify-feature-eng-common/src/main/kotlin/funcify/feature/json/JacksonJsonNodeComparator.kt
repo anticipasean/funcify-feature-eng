8bba2371273cbbdd6cfbdd8659075c1f42f36385
anticipasean <spmccarron@gmail.com>
2022-07-09 11:44:13 -0400

    0.0.x: Add more documentation to jackson_json_node_comparator implementation stating biases before the details are forgotten

--- funcify-feature-eng-common/src/main/kotlin/funcify/feature/json/JacksonJsonNodeComparator.kt
+++ funcify-feature-eng-common/src/main/kotlin/funcify/feature/json/JacksonJsonNodeComparator.kt
@@ -6,6 +6,33 @@ import com.fasterxml.jackson.databind.node.*
 
 object JacksonJsonNodeComparator : Comparator<JsonNode> {
 
+    /**
+     * Biased [JsonNodeType] comparator:
+     * - null or missing values are treated as equal and go before scalars
+     * - scalars(*) (i.e. Booleans, Numerics, and Strings) before container types (i.e. Arrays,
+     * Objects)
+     * - [BinaryNode]s (=> [ByteArray]s ) before [ArrayNode]s (=> [JsonNode] arrays)
+     * - arrays before objects(^)
+     *
+     * (*) The reasoning behind this: **Scalar** here would be any value of a type not requiring
+     * _explicit_ iteration over its parts to assess equality or deduce a value for ordering
+     * instances. [String]s _technically can_ be iterated over when treated as character arrays but
+     * [String]'s implementation of the [Comparable] contract at least comes standard in Java and
+     * Kotlin, whereas those of ByteArray, ArrayNode, and ObjectNode do not
+     *
+     * (^) The reasoning behind this: Arrays are treated in these comparisons as an iterable of
+     * pairs with each pair's first value representing the index value e.g. `[ (0, "cake"), (1,
+     * "frosting"), ... ]` and objects are treated in comparison as an iterable of pairs with each
+     * pair's first value representing the [String] key used to get the [JsonNode] value. Array
+     * indices in a such an iterable `[0..n-1]` would then lexicographically come before object keys
+     * `["cake", "frosting"]` when compared side-by-side.
+     * ```
+     *  >>> [str(0),str(1)] > ["cake", "frosting"]
+     *  False
+     *  >>> [str(0),str(1)] < ["cake", "frosting"]
+     *  True
+     * ```
+     */
     private val nodeTypeComparator: Comparator<JsonNodeType> by lazy {
         val nodeTypeOrderMap: Map<JsonNodeType, Int> by lazy {
             JsonNodeType.values()
@@ -19,10 +46,11 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
                             JsonNodeType.NULL, JsonNodeType.MISSING -> 0
                             JsonNodeType.BOOLEAN -> 1
                             JsonNodeType.NUMBER -> 2
-                            JsonNodeType.STRING, JsonNodeType.BINARY -> 3
-                            JsonNodeType.ARRAY -> 4
-                            JsonNodeType.OBJECT -> 5
-                            JsonNodeType.POJO -> 6
+                            JsonNodeType.STRING -> 3
+                            JsonNodeType.BINARY -> 4
+                            JsonNodeType.ARRAY -> 5
+                            JsonNodeType.OBJECT -> 6
+                            JsonNodeType.POJO -> 7
                         }
                 }
                 .toMap()
@@ -33,6 +61,17 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
     private val booleanNodeScalarComparator: Comparator<BooleanNode> by lazy {
         Comparator { o1, o2 -> o1.booleanValue().compareTo(o2.booleanValue()) }
     }
+
+    /**
+     * Biased numeric node comparator:
+     * - if their numeric type information is missing/null, then apply _null first_ comparison logic
+     * - if both numeric nodes represent _integral_ numbers, compare them as [java.math.BigInteger]s
+     * - if either node represents a floating point number, compare them as [java.math.BigDecimal]s
+     * in order to avoid any loss of information a conversion to a binary floating point number
+     * might incur and leverage [java.math.BigDecimal]'s Comparable contract
+     * - if either node represents a NaN in a floating point number type, treat NaN as missing/null
+     * values would be treated
+     */
     private val numericNodeComparator: Comparator<NumericNode> by lazy {
         Comparator { o1, o2 ->
             when (o1.numberType()) {
@@ -55,14 +94,26 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
                         JsonParser.NumberType.FLOAT,
                         JsonParser.NumberType.DOUBLE,
                         JsonParser.NumberType.BIG_DECIMAL -> {
-                            o1.decimalValue().compareTo(o2.decimalValue())
+                            if (o2.isNaN) {
+                                1
+                            } else {
+                                o1.decimalValue().compareTo(o2.decimalValue())
+                            }
                         }
                     }
                 }
                 JsonParser.NumberType.FLOAT,
                 JsonParser.NumberType.DOUBLE,
                 JsonParser.NumberType.BIG_DECIMAL -> {
-                    o1.decimalValue().compareTo(o2.decimalValue())
+                    if (o1.isNaN || o2.isNaN) {
+                        when {
+                            o1.isNaN && o2.isNaN -> 0
+                            o1.isNaN -> -1
+                            else -> 1
+                        }
+                    } else {
+                        o1.decimalValue().compareTo(o2.decimalValue())
+                    }
                 }
             }
         }
@@ -73,13 +124,9 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
 
     private val binaryNodeScalarComparator: Comparator<BinaryNode> by lazy {
         Comparator { o1, o2 ->
-            // The smaller bytearray will go first if the byte arrays match up to the smaller one's
+            // The smaller byte array will go first if the byte arrays match up to the smaller one's
             // n-1 node
-            val sizeComparisonBias: Int =
-                when (val sizeComparison = o1.binaryValue().size.compareTo(o2.binaryValue().size)) {
-                    0 -> 0
-                    else -> sizeComparison
-                }
+            val sizeComparisonBias: Int = o1.binaryValue().size.compareTo(o2.binaryValue().size)
             o1.binaryValue()
                 .asSequence()
                 .zip(o2.binaryValue().asSequence()) { b1, b2 -> b1.compareTo(b2) }
@@ -99,8 +146,9 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
         return compareNonNullNodes(o1, o2)
     }
 
-    private fun compareNonNullNodes(o1: JsonNode, o2: JsonNode) =
-        when (val nodeTypeComparison = nodeTypeComparator.compare(o1.nodeType, o2.nodeType)) {
+    private fun compareNonNullNodes(o1: JsonNode, o2: JsonNode): Int {
+        return when (val nodeTypeComparison = nodeTypeComparator.compare(o1.nodeType, o2.nodeType)
+        ) {
             0 -> {
                 compareSimilarNodeTypes(o1, o2)
             }
@@ -108,6 +156,7 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
                 nodeTypeComparison
             }
         }
+    }
 
     private fun compareSimilarNodeTypes(o1: JsonNode, o2: JsonNode): Int {
         when {
@@ -157,31 +206,42 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
         return binaryNodeScalarComparator.compare(o1, o2)
     }
 
+    /**
+     * Biases:
+     * - Assume order of entries is significant, so no sorting is performed before comparison
+     * - [JsonNode] _elements_ cannot be null and are replaced with [NullNode]s by Jackson code when
+     * performing updates on a given [ArrayNode] (If Jackson's API fails to do this for any case,
+     * comparison could result in a [NullPointerException])
+     */
     private fun compareArrayNodes(o1: ArrayNode, o2: ArrayNode): Int {
         // The smaller array_node will go first if the two array_nodes match up to the smaller
         // node's n-1 json_node
-        val sizeComparisonBias: Int =
-            when (val sizeComparison = o1.size().compareTo(o2.size())) {
-                0 -> 0
-                else -> sizeComparison
-            }
+        val sizeComparisonBias: Int = o1.size().compareTo(o2.size())
         return o1.asSequence()
             .zip(o2.asSequence()) { n1, n2 ->
-                // recursive call
+                // potentially recursive call if both nodes are array_nodes
                 compareNonNullNodes(n1, n2)
             }
             .firstOrNull { comp -> comp != 0 }
             ?: sizeComparisonBias
     }
 
+    /**
+     * Biases:
+     * - Assume order of entries is significant and do not sort entries by [String] keys before
+     * comparison. Jackson [ObjectNode]s are backed by [LinkedHashMap]s that preserve insertion
+     * order so the order of the entries is not _random_ or based on hash code values of the keys
+     * (as it would be if the backing [Map] container type were [HashMap])
+     * - [String] _keys_ can be "null" and null keys come first
+     * - [JsonNode] _values_ cannot be null and are replaced with [NullNode]s by Jackson code when
+     * performing updates on a given [ObjectNode] (If Jackson's API fails to do this for any case,
+     * comparison could result in a [NullPointerException])
+     * - _keys_ are compared before _values_ by entry-to-entry and **not** set-to-set
+     */
     private fun compareObjectNodes(o1: ObjectNode, o2: ObjectNode): Int {
         // The smaller object_node will go first if the two object_node's keys and values match up
         // to the smaller one's n-1 entry
-        val sizeComparisonBias: Int =
-            when (val sizeComparison = o1.size().compareTo(o2.size())) {
-                0 -> 0
-                else -> sizeComparison
-            }
+        val sizeComparisonBias: Int = o1.size().compareTo(o2.size())
         return o1.fields()
             .asSequence()
             .zip(o2.fields().asSequence()) { (k1, v1), (k2, v2) ->
@@ -194,7 +254,7 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
                 } else {
                     when (val keyComparison = k1.compareTo(k2)) {
                         0 -> {
-                            // recursive call
+                            // potentially recursive call if both nodes are object_nodes
                             compareNonNullNodes(v1, v2)
                         }
                         else -> {
@@ -208,10 +268,20 @@ object JacksonJsonNodeComparator : Comparator<JsonNode> {
     }
 
     private fun comparePojoNodes(o1: POJONode, o2: POJONode): Int {
-        // Not much that can be done without any clear type information or use cases
-        return when {
-            o1.pojo == o2.pojo -> 0
-            else -> o1.toString().compareTo(o2.toString())
+        return when (o1) {
+            o2 -> 0
+            else ->
+                // Not much else that can be done without any clear type information or use case
+                // If Jackson is unable to serialize these nodes into JSON strings, then throw an
+                // error indicating that
+                try {
+                    o1.toString().compareTo(o2.toString())
+                } catch (t: Throwable) {
+                    throw IllegalStateException(
+                        "unable to compare these two ${POJONode::class.qualifiedName} instances without error",
+                        t
+                    )
+                }
         }
     }
 }
