34011a628f18a1a226bb01ae843bc68568ae67dc
anticipasean <spmccarron@gmail.com>
2022-07-31 16:24:47 -0400

    0.0.x: Fix issue with none of the datasource index specific strategies not being applied due to bad vertex type to graph vertex type matching logic

--- funcify-feature-eng-schema/src/main/kotlin/funcify/feature/schema/vertex/SchematicGraphVertexType.kt
+++ funcify-feature-eng-schema/src/main/kotlin/funcify/feature/schema/vertex/SchematicGraphVertexType.kt
@@ -3,17 +3,18 @@ package funcify.feature.schema.vertex
 import arrow.core.Option
 import arrow.core.toOption
 import funcify.feature.schema.SchematicVertex
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSubclassOf
-import kotlinx.collections.immutable.ImmutableMap
+import kotlin.reflect.full.isSuperclassOf
 import kotlinx.collections.immutable.ImmutableSet
-import kotlinx.collections.immutable.persistentMapOf
 import kotlinx.collections.immutable.toPersistentSet
 
 /**
- * Enum enabling graph vertex types to be handled in `when(graphVertexType) {...}` case structures without
- * compromising the API, as "sealing" vertex types would likely make the API less flexible while
- * it's still undergoing lots of changes
+ * Enum enabling graph vertex types to be handled in `when(graphVertexType) {...}` case structures
+ * without compromising the API, as "sealing" vertex types would likely make the API less flexible
+ * while it's still undergoing lots of changes
  * @author smccarron
  * @created 2022-06-29
  */
@@ -25,17 +26,35 @@ enum class SchematicGraphVertexType(val graphVertexSubtype: KClass<out Schematic
     PARAMETER_LEAF_VERTEX(ParameterLeafVertex::class);
 
     companion object {
-        val schematicGraphVertexTypeByVertexSubtype:
-            ImmutableMap<KClass<out SchematicVertex>, SchematicGraphVertexType> by lazy {
-            SchematicGraphVertexType.values().fold(persistentMapOf()) { pm, sgvt ->
-                pm.put(sgvt.graphVertexSubtype, sgvt)
+        /**
+         * Memoizes matched schematic vertex subtypes with their corresponding schematic graph
+         * vertex type
+         */
+        private val schematicGraphVertexTypeByVertexSubtypeMemoizer:
+            (KClass<out SchematicVertex>) -> Option<SchematicGraphVertexType> by lazy {
+            val backingConcurrentMap:
+                ConcurrentMap<KClass<out SchematicVertex>, SchematicGraphVertexType> =
+                ConcurrentHashMap()
+            val schematicVertexSubtypeMatcher:
+                (KClass<out SchematicVertex>) -> SchematicGraphVertexType? =
+                { schematicVertexType: KClass<out SchematicVertex> ->
+                    SchematicGraphVertexType.values().asSequence().firstOrNull { vertexType ->
+                        vertexType.graphVertexSubtype.isSuperclassOf(schematicVertexType)
+                    }
+                }
+            { schematicVertexType: KClass<out SchematicVertex> ->
+                backingConcurrentMap
+                    .computeIfAbsent(schematicVertexType, schematicVertexSubtypeMatcher)
+                    .toOption()
             }
         }
+
         fun getSchematicGraphTypeForVertexSubtype(
             vertexSubtype: KClass<out SchematicVertex>
         ): Option<SchematicGraphVertexType> {
-            return schematicGraphVertexTypeByVertexSubtype[vertexSubtype].toOption()
+            return schematicGraphVertexTypeByVertexSubtypeMemoizer.invoke(vertexSubtype)
         }
+
         private val containerTypeVertexTypes: ImmutableSet<SchematicGraphVertexType> by lazy {
             SchematicGraphVertexType.values()
                 .asSequence()
