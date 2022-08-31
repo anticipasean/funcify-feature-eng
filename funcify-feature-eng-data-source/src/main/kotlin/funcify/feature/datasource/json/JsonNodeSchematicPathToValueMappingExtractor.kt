5e0af02f088e65c0429df5b9b88669a52c0fd570
anticipasean <spmccarron@gmail.com>
2022-08-31 18:11:02 -0400

    0.0.x: Add support for indexing within schematic_paths mapping results within a json_response for array_nodes

--- funcify-feature-eng-data-source/src/main/kotlin/funcify/feature/datasource/json/JsonNodeSchematicPathToValueMappingExtractor.kt
+++ funcify-feature-eng-data-source/src/main/kotlin/funcify/feature/datasource/json/JsonNodeSchematicPathToValueMappingExtractor.kt
@@ -3,10 +3,12 @@ package funcify.feature.datasource.json
 import arrow.core.Either
 import arrow.core.filterIsInstance
 import arrow.core.identity
+import arrow.core.lastOrNone
 import arrow.core.left
 import arrow.core.right
 import arrow.core.toOption
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.JsonNodeType
 import com.fasterxml.jackson.databind.node.ObjectNode
 import funcify.feature.schema.path.SchematicPath
@@ -39,12 +41,46 @@ object JsonNodeSchematicPathToValueMappingExtractor :
             JsonNodeType.MISSING,
             JsonNodeType.NUMBER,
             JsonNodeType.BINARY,
-            JsonNodeType.STRING,
-            JsonNodeType.ARRAY -> {
+            JsonNodeType.STRING -> {
                 sequenceOf(
                     (SchematicPath.of { pathSegments(context.pathSegments) } to context.value)
                         .right()
                 )
+            }
+            JsonNodeType.ARRAY -> {
+                context.pathSegments
+                    .toOption()
+                    .filter { ps -> ps.isNotEmpty() }
+                    .map { ps -> (SchematicPath.of { pathSegments(ps) } to context.value).right() }
+                    .fold(::emptySequence, ::sequenceOf)
+                    .plus(
+                        context.value
+                            .toOption()
+                            .filterIsInstance<ArrayNode>()
+                            .map { an -> an.asSequence().withIndex() }
+                            .fold(::emptySequence, ::identity)
+                            .map { indexedValue: IndexedValue<JsonNode> ->
+                                JsonNodePathTraversalContext(
+                                        pathSegments =
+                                            context.pathSegments
+                                                .lastOrNone()
+                                                .map { s ->
+                                                    StringBuilder(s)
+                                                        .append('[')
+                                                        .append(indexedValue.index)
+                                                        .append(']')
+                                                        .toString()
+                                                }
+                                                .fold(::persistentListOf) { lastSegment ->
+                                                    context.pathSegments
+                                                        .removeAt(context.pathSegments.size - 1)
+                                                        .add(lastSegment)
+                                                },
+                                        value = indexedValue.value
+                                    )
+                                    .left()
+                            }
+                    )
             }
             JsonNodeType.OBJECT -> {
                 context.pathSegments
