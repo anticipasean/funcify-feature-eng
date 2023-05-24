4360f7cfbca397fe0f0c7d5d396175b46155cf10
anticipasean <spmccarron@gmail.com>
2023-05-24 14:49:56 -0400

    schema-v2: Continue modeling possible schema-v2

--- funcify-feature-eng-fntree/src/main/kotlin/funcify/feature/fntree/GQLTreeFunction.kt
+++ funcify-feature-eng-fntree/src/main/kotlin/funcify/feature/fntree/GQLTreeFunction.kt
@@ -1,8 +1,20 @@
 package funcify.feature.fntree
 
+import graphql.schema.GraphQLArgument
+import graphql.schema.GraphQLFieldDefinition
+import graphql.schema.GraphQLOutputType
+import kotlinx.collections.immutable.ImmutableMap
+
 /**
  *
  * @author smccarron
  * @created 2023-04-05
  */
-interface GQLTreeFunction {}
+interface GQLTreeFunction {
+
+    val fieldDefinition: GraphQLFieldDefinition
+
+    val inputArgumentsByName: ImmutableMap<String, GraphQLArgument>
+
+    val outputType: GraphQLOutputType
+}
