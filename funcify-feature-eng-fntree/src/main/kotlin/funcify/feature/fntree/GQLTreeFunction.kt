e1bed39abe28e62d4fe11934c938963199d59730
anticipasean <spmccarron@gmail.com>
2023-06-09 18:02:46 -0400

    schema-v2: Add sample dataset and schema for use in determining what's needed in beam project

--- funcify-feature-eng-fntree/src/main/kotlin/funcify/feature/fntree/GQLTreeFunction.kt
+++ funcify-feature-eng-fntree/src/main/kotlin/funcify/feature/fntree/GQLTreeFunction.kt
@@ -1,20 +1,23 @@
 package funcify.feature.fntree
 
+import com.fasterxml.jackson.databind.JsonNode
 import graphql.schema.GraphQLArgument
-import graphql.schema.GraphQLFieldDefinition
 import graphql.schema.GraphQLOutputType
 import kotlinx.collections.immutable.ImmutableMap
+import reactor.core.publisher.Mono
 
 /**
- *
  * @author smccarron
  * @created 2023-04-05
  */
-interface GQLTreeFunction {
+interface GQLTreeFunction : (JsonNode) -> Mono<JsonNode> {
 
-    val fieldDefinition: GraphQLFieldDefinition
+    val fieldName: String
 
     val inputArgumentsByName: ImmutableMap<String, GraphQLArgument>
 
     val outputType: GraphQLOutputType
+
+    override fun invoke(jsonNode: JsonNode): Mono<JsonNode>
+
 }
