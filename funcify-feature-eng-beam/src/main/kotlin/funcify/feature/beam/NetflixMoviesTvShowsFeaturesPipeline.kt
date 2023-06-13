445075272a10c5aa27641493f2a53f04646c9a56
anticipasean <spmccarron@gmail.com>
2023-06-13 17:54:12 -0400

    schema-v2: Continue exploring bare minimum of what is needed to stand up pipeline with GraphQL setup

--- funcify-feature-eng-beam/src/main/kotlin/funcify/feature/beam/NetflixMoviesTvShowsFeaturesPipeline.kt
+++ funcify-feature-eng-beam/src/main/kotlin/funcify/feature/beam/NetflixMoviesTvShowsFeaturesPipeline.kt
@@ -1,8 +1,32 @@
 package funcify.feature.beam
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import funcify.feature.scalar.registry.ScalarTypeRegistry
+import funcify.feature.tools.container.attempt.Try
+import graphql.ExceptionWhileDataFetching
+import graphql.ExecutionInput
+import graphql.ExecutionResult
+import graphql.GraphQL
+import graphql.GraphQLError
+import graphql.execution.DataFetcherResult
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+import graphql.schema.GraphQLScalarType
+import graphql.schema.GraphQLSchema
+import graphql.schema.idl.FieldWiringEnvironment
+import graphql.schema.idl.RuntimeWiring
+import graphql.schema.idl.ScalarWiringEnvironment
+import graphql.schema.idl.SchemaGenerator
+import graphql.schema.idl.SchemaParser
+import graphql.schema.idl.TypeDefinitionRegistry
+import graphql.schema.idl.WiringFactory
+import java.io.File
 import java.io.Reader
 import java.nio.channels.Channels
 import java.nio.charset.Charset
+import java.nio.file.Files
+import java.util.concurrent.CompletableFuture
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.io.FileIO
 import org.apache.beam.sdk.io.FileIO.ReadableFile
@@ -12,20 +36,24 @@ import org.apache.beam.sdk.options.Description
 import org.apache.beam.sdk.options.PipelineOptions
 import org.apache.beam.sdk.options.PipelineOptionsFactory
 import org.apache.beam.sdk.options.Validation
+import org.apache.beam.sdk.transforms.Contextful
 import org.apache.beam.sdk.transforms.FlatMapElements
 import org.apache.beam.sdk.transforms.InferableFunction
+import org.apache.beam.sdk.transforms.MapElements
+import org.apache.beam.sdk.transforms.SerializableFunction
 import org.apache.beam.sdk.values.PDone
 import org.apache.beam.sdk.values.TypeDescriptor
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
-import org.apache.commons.csv.CSVPrinter
 import org.apache.commons.csv.CSVRecord
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 object NetflixMoviesTvShowsFeaturesPipeline {
 
     class ConvertIntoCsvRecordsFn(
         private val charset: Charset = Charsets.UTF_8,
-        private val csvFormat: CSVFormat = CSVFormat.DEFAULT
+        private val csvFormat: CSVFormat = CSVFormat.DEFAULT,
     ) : InferableFunction<ReadableFile, Iterable<CSVRecord>>() {
 
         @Throws(Exception::class)
@@ -36,11 +64,136 @@ object NetflixMoviesTvShowsFeaturesPipeline {
         }
     }
 
-    class ConvertCSVRecordIntoTextFn() : InferableFunction<CSVRecord, String>() {
+    class GraphQLResource(val charset: Charset = Charsets.UTF_8) : (File) -> GraphQLSchema {
+
+        override fun invoke(schemaFile: File): GraphQLSchema {
+            return Try.attempt {
+                    SchemaParser().parse(Files.newBufferedReader(schemaFile.toPath(), charset))
+                }
+                .flatMap { tdr: TypeDefinitionRegistry ->
+                    val errorHolder: Array<GraphQLError?> = arrayOfNulls<GraphQLError>(1)
+                    tdr.addAll(
+                            ScalarTypeRegistry.materializationRegistry().getAllScalarDefinitions()
+                        )
+                        .ifPresent { ge: GraphQLError -> errorHolder[0] = ge }
+                    if (errorHolder[0] != null) {
+                        Try.failure(
+                            RuntimeException("graphql_error: [ %s ]".format(errorHolder[0]))
+                        )
+                    } else {
+                        Try.success(tdr)
+                    }
+                }
+                .map { tdr: TypeDefinitionRegistry ->
+                    SchemaGenerator()
+                        .makeExecutableSchema(
+                            tdr,
+                            RuntimeWiring.newRuntimeWiring()
+                                .wiringFactory(PipelineWiringFactory())
+                                .build()
+                        )
+                }
+                .orElseThrow()
+        }
+    }
+
+    class PipelineWiringFactory() : WiringFactory {
+
+        companion object {
+            private val logger: Logger = LoggerFactory.getLogger(PipelineWiringFactory::class.java)
+        }
+
+        override fun providesScalar(environment: ScalarWiringEnvironment): Boolean {
+            logger.info(
+                "provides_scalar: [ env.scalar_type_definition.name: {} ]",
+                environment.scalarTypeDefinition.name
+            )
+            return ScalarTypeRegistry.materializationRegistry()
+                .getScalarTypeDefinitionWithName(environment.scalarTypeDefinition.name) != null
+        }
+
+        override fun getScalar(environment: ScalarWiringEnvironment): GraphQLScalarType {
+            return ScalarTypeRegistry.materializationRegistry()
+                .getGraphQLScalarTypeWithName(environment.scalarTypeDefinition.name)!!
+        }
+
+        override fun providesDataFetcher(environment: FieldWiringEnvironment): Boolean {
+            logger.info(
+                "provides_data_fetcher: [ env.field_definition.name: {} ]",
+                environment.fieldDefinition.name
+            )
+            return true
+        }
+
+        override fun getDataFetcher(environment: FieldWiringEnvironment): DataFetcher<*> {
+            return PipelineDataFetcher()
+        }
+    }
+
+    class PipelineDataFetcher() : DataFetcher<CompletableFuture<DataFetcherResult<Any?>>> {
+
+        companion object {
+            private val logger: Logger = LoggerFactory.getLogger(PipelineDataFetcher::class.java)
+        }
+
+        override fun get(
+            environment: DataFetchingEnvironment
+        ): CompletableFuture<DataFetcherResult<Any?>> {
+            logger.info(
+                "get: [ env.field.name: {}, local_context: {} ]",
+                environment.field.name,
+                environment.getLocalContext()
+            )
+            return CompletableFuture.completedFuture(
+                DataFetcherResult.newResult<Any?>()
+                    .error(
+                        ExceptionWhileDataFetching(
+                            environment.executionStepInfo.path,
+                            IllegalArgumentException("logic not supported"),
+                            environment.field.sourceLocation
+                        )
+                    )
+                    .build()
+            )
+        }
+    }
+
+    class ConvertIntoJSONFn(
+        private val graphQL: GraphQL,
+        private val objectMapper: ObjectMapper = ObjectMapper()
+    ) : InferableFunction<CSVRecord, JsonNode>() {
+
+        companion object {
+            val QUERY: String =
+                """
+                |query showFeatures(${'$'}showId: ID!){
+                |    show(showId: ${'$'}{showId}) {
+                |        showId
+                |        title
+                |        releaseYear
+                |        director
+                |        cast
+                |        audienceSuitabilityRating
+                |        productionCountry
+                |        genres
+                |        dateAdded
+                |    }
+                |}
+            """
+                    .trimMargin()
+        }
 
         @Throws(Exception::class)
-        override fun apply(input: CSVRecord): String {
-            return CSVPrinter()
+        override fun apply(input: CSVRecord): JsonNode {
+            return graphQL
+                .executeAsync(
+                    ExecutionInput.newExecutionInput()
+                        .localContext(input)
+                        .query(QUERY)
+                        .variables(mapOf("showId" to input.get("showId")))
+                )
+                .join()
+                .let { er: ExecutionResult -> objectMapper.valueToTree(er.toSpecification()) }
         }
     }
 
@@ -59,6 +212,10 @@ object NetflixMoviesTvShowsFeaturesPipeline {
         @get:Description("Format as defined in org.apache.commons.csv.CSVFormat.Predefined")
         @get:Default.String("DEFAULT")
         var csvFormat: String
+
+        @get:Description("Path of the graphqls schema file")
+        @get:Default.String("netflix_movies_and_tv_shows.graphqls")
+        var graphQLSchemaFile: String
 
         @get:Description("Path of the file to write to")
         @get:Validation.Required //
@@ -81,15 +238,27 @@ object NetflixMoviesTvShowsFeaturesPipeline {
                 }
                 ?.format
                 ?: CSVFormat.DEFAULT
+
+        val graphQLSchema: GraphQLSchema = GraphQLResource().invoke(File(options.graphQLSchemaFile))
         val p: Pipeline = Pipeline.create(options)
+
         p.apply("MatchFilePattern", FileIO.match().filepattern(options.inputFile))
             .apply("ReadMatchedFile", FileIO.readMatches())
             .apply(
                 "ConvertIntoCsvRecords",
                 FlatMapElements.into(TypeDescriptor.of(CSVRecord::class.java))
-                    .via(ConvertIntoCsvRecordsFn(charset, csvFormat))
+                    .via(Contextful.fn(ConvertIntoCsvRecordsFn(charset, csvFormat)))
             )
-            .apply<PDone>("WriteCounts", TextIO.write().to(options.output))
+            .apply(
+                "ConvertIntoJSON",
+                MapElements.into(TypeDescriptor.of(JsonNode::class.java))
+                    .via(ConvertIntoJSONFn(GraphQL.newGraphQL(graphQLSchema).build()))
+            )
+            .apply(
+                MapElements.into(TypeDescriptor.of(String::class.java))
+                    .via(SerializableFunction { jn: JsonNode -> jn.toString() })
+            )
+            .apply<PDone>("WriteJSON", TextIO.write().to(options.output))
 
         p.run().waitUntilFinish()
     }
