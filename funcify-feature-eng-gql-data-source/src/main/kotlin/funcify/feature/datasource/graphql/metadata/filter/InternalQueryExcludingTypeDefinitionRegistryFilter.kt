061fa0ee50ecb25c7b6dc6d31ffe62e9576ab40c
anticipasean <spmccarron@gmail.com>
2023-06-29 21:46:05 -0400

    schema-v2: Add logging to new internal_query_excluding_type_definition_registry_filter

--- funcify-feature-eng-gql-data-source/src/main/kotlin/funcify/feature/datasource/graphql/metadata/filter/InternalQueryExcludingTypeDefinitionRegistryFilter.kt
+++ funcify-feature-eng-gql-data-source/src/main/kotlin/funcify/feature/datasource/graphql/metadata/filter/InternalQueryExcludingTypeDefinitionRegistryFilter.kt
@@ -1,22 +1,40 @@
 package funcify.feature.datasource.graphql.metadata.filter
 
 import funcify.feature.error.ServiceError
+import funcify.feature.tools.extensions.LoggerExtensions.loggerFor
 import graphql.GraphQLError
 import graphql.language.FieldDefinition
 import graphql.language.ObjectTypeDefinition
 import graphql.schema.idl.TypeDefinitionRegistry
+import org.slf4j.Logger
 
 /**
  * @author smccarron
  * @created 2023-06-29
  */
-class InternalQueryExcludingTypeDefinitionRegistryFilter : TypeDefinitionRegistryFilter {
+class InternalQueryExcludingTypeDefinitionRegistryFilter() : TypeDefinitionRegistryFilter {
+
+    companion object {
+        private const val QUERY_OBJECT_TYPE_NAME = "Query"
+        private val logger: Logger = loggerFor<InternalQueryExcludingTypeDefinitionRegistryFilter>()
+    }
 
     override fun filter(
         typeDefinitionRegistry: TypeDefinitionRegistry
     ): Result<TypeDefinitionRegistry> {
+        if (logger.isDebugEnabled) {
+            logger.debug(
+                "filter: [ type_definition_registry.get_type({}, {}): {} ]",
+                QUERY_OBJECT_TYPE_NAME,
+                ObjectTypeDefinition::class.java.name,
+                typeDefinitionRegistry.getType(
+                    QUERY_OBJECT_TYPE_NAME,
+                    ObjectTypeDefinition::class.java
+                )
+            )
+        }
         return typeDefinitionRegistry
-            .getType("Query", ObjectTypeDefinition::class.java)
+            .getType(QUERY_OBJECT_TYPE_NAME, ObjectTypeDefinition::class.java)
             .flatMap { otd: ObjectTypeDefinition ->
                 val updatedDef: ObjectTypeDefinition =
                     otd.transform { otdb: ObjectTypeDefinition.Builder ->
