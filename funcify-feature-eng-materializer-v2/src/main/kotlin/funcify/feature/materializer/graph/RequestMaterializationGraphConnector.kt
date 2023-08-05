fde470da33ab0b3ac29446dfa029220d16eff766
anticipasean <spmccarron@gmail.com>
2023-08-05 12:15:54 -0400

    schema-v2: Update connector interface for request materialization graph wiring

--- funcify-feature-eng-materializer-v2/src/main/kotlin/funcify/feature/materializer/graph/RequestMaterializationGraphConnector.kt
+++ funcify-feature-eng-materializer-v2/src/main/kotlin/funcify/feature/materializer/graph/RequestMaterializationGraphConnector.kt
@@ -1,20 +1,55 @@
 package funcify.feature.materializer.graph
 
+import funcify.feature.schema.path.GQLOperationPath
+import graphql.language.Argument
+import graphql.language.Field
+import graphql.language.OperationDefinition
+
 /**
- *
  * @author smccarron
  * @created 2023-08-02
  */
 interface RequestMaterializationGraphConnector<C> {
 
-    fun connectOperationDefinitionToFieldArgument(context: C): C
+    fun createOperationDefinition(context: C): C
 
-    fun connectOperationDefinitionToField(context: C): C
+    fun connectOperationDefinitionToFieldArgument(
+        context: C,
+        opDefPath: GQLOperationPath,
+        operationDefinition: OperationDefinition,
+        fieldArgPath: GQLOperationPath,
+        argument: Argument
+    ): C
 
-    fun connectFieldToFieldArgument(context: C): C
+    fun connectOperationDefinitionToField(
+        context: C,
+        opDefPath: GQLOperationPath,
+        operationDefinition: OperationDefinition,
+        fieldPath: GQLOperationPath,
+        field: Field
+    ): C
 
-    fun connectFieldArgumentToField(context: C): C
+    fun connectFieldToFieldArgument(
+        context: C,
+        fieldPath: GQLOperationPath,
+        field: Field,
+        fieldArgPath: GQLOperationPath,
+        argument: Argument
+    ): C
 
-    fun connectFieldToField(context: C): C
+    fun connectFieldArgumentToField(
+        context: C,
+        fieldArgPath: GQLOperationPath,
+        argument: Argument,
+        fieldPath: GQLOperationPath,
+        field: Field
+    ): C
 
+    fun connectFieldToField(
+        context: C,
+        fieldPath1: GQLOperationPath,
+        field1: Field,
+        fieldPath2: GQLOperationPath,
+        field2: Field
+    ): C
 }
